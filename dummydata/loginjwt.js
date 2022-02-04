f056d6ecb5b5577f25c5ba52650a2b981cfefa21
lorisunjunbin <6099012@qq.com>
2022-02-04 22:40:56 +0800

    clean & support cors.

---
+++ dummydata/loginjwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+    "token": "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJidXJha3RhcyIsImV4cCI6MTU3NDI3NDI0OCwiaWF0IjoxNTc0MjUyNjQ4fQ.Y__b_QdlJq_MeF5mwy2UhAq-cX6QbikS4a_oCDkGwpb0UrYDV5Vtw0KBJWHQcp_qX3yrnDMjsM4eIXtp03KHBA"
+}
--- index.js
+++ index.js
@@ -1,14 +1,27 @@
-const port = 3000
+const port = 3323
 const express = require('express');
-const app = express();
 
-const dummyData = {
-    "products": require('./dummydata/products'),
-    "categories": require('./dummydata/categories'),
-    "define_dummy_data": "in_folder_dummydata"
+const cors = require("cors");
+const app = express();
+app.use(cors());
+
+const static_path = __dirname.replace('server', 'public/');
+
+//route
+const routes = {
+    "services": "index.html",
+    "products": "index.html",
+    "sign-up": "index.html",
+    "log-in": "index.html"
 };
 
-app.use(express.static(__dirname + '/public'));
+//dummy data
+const dummyData = {
+    "authenticate": require('./dummydata/loginjwt'),
+    "products_data": require('./dummydata/products'), "define_dummy_data": "in_folder_dummydata"
+};
+
+app.use(express.static('../build'));
 
 app.get('/metadata', (req, res) => {
     console.log('return dummy - metadata ')
@@ -16,21 +29,54 @@ app.get('/metadata', (req, res) => {
 })
 
 app.get("/:param", (req, res) => {
-    const data = dummyData[req.params['param']];
-    if (!!data) {
-        console.log('Dummy Data -------------------------------->> ' + req.params['param'])
-        console.log(data)
-        console.log('Dummy Data --------------------------------<< ' + req.params['param'])
-        res.send(data);
+    console.log('app.get');
+    //route
+    if (routes[req.params['param']]) {
+        let file_path = static_path + routes[req.params['param']];
+        console.log('route to file: ' + file_path)
+        res.sendFile(file_path);
+        //data
     } else {
-        console.log('return status: 412 - ' + req.params['param'])
-        res.sendStatus(412)
+        const data = dummyData[req.params['param']];
+        if (!!data) {
+            console.log('Dummy Data -------------------------------->> ' + req.params['param'])
+            console.log(data)
+            console.log('Dummy Data --------------------------------<< ' + req.params['param'])
+            res.setHeader('Access-Control-Allow-Origin','*');
+            res.send(data);
+        } else {
+            console.log('return status: 412 - ' + req.params['param'])
+            res.sendStatus(412)
+        }
+    }
+})
+
+app.post("/:param", (req, res) => {
+    console.log('app.post');
+    //route
+    if (routes[req.params['param']]) {
+        let file_path = static_path + routes[req.params['param']];
+        console.log('route to file: ' + file_path)
+        res.sendFile(file_path);
+        //data
+    } else {
+        const data = dummyData[req.params['param']];
+        if (!!data) {
+            console.log('Dummy Data -------------------------------->> ' + req.params['param'])
+            console.log(data)
+            console.log('Dummy Data --------------------------------<< ' + req.params['param'])
+            res.setHeader('Access-Control-Allow-Origin','*');
+            res.send(data);
+        } else {
+            console.log('return status: 412 - ' + req.params['param'])
+            res.sendStatus(412)
+        }
     }
 })
 
 app.listen(port, () => {
     console.log(`rest server listening at http://localhost:${port}/metadata`)
-    for (key in dummyData) {
-        console.log(`Dummy ${key} -> http://localhost:${port}/${key}`)
+    for (k in dummyData) {
+        console.log(`Dummy ${k} -> http://localhost:${port}/${k}`)
     }
 })
